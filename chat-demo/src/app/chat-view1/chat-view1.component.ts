7598ee7f9ffa2d0ae450b994dafc65de7d6b229f
Michael Rosario <michael.p.rosario@gmail.com>
2024-09-01 12:27:30 +0000

    chore: Update chat view 1 component to display chat messages and allow sending messages

--- chat-demo/src/app/chat-view1/chat-view1.component.ts
+++ chat-demo/src/app/chat-view1/chat-view1.component.ts
@@ -1,12 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface IMessage {
+  sender: string;
+  content: string;
+  time: string;
+}
 
 @Component({
   selector: 'app-chat-view1',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './chat-view1.component.html',
   styleUrl: './chat-view1.component.scss'
 })
 export class ChatView1Component {
 
+  // create a list of messages using IMessage interface.  Make 5 messages
+  messages: IMessage[] = [
+    { sender: 'John', content: 'Hello', time: '12:00' },
+    { sender: 'Jane', content: 'Hi', time: '12:01' },
+    { sender: 'John', content: 'How are you?', time: '12:02' },
+    { sender: 'Jane', content: 'I am good, thank you', time: '12:03' },
+    { sender: 'John', content: 'That is great to hear', time: '12:04' }
+  ];
+  messageContent: string = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  sendMessage() { 
+    alert(this.messageContent)
+  }
 }

a3fa28e053ecf3742dd80c4640146b8e13523e3b
Michael Rosario <michael.p.rosario@gmail.com>
2024-09-01 12:12:49 +0000

    feat ( chat view 1 ) add component

--- chat-demo/src/app/app.routes.ts
+++ chat-demo/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ChatView1Component } from './chat-view1/chat-view1.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: ChatView1Component },
+];
---
+++ chat-demo/src/app/chat-view1/chat-view1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-view1',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat-view1.component.html',
+  styleUrl: './chat-view1.component.scss'
+})
+export class ChatView1Component {
+
+}

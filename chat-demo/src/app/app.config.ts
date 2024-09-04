372b69a7d825888fbdf7a6f8b84da06858583cce
Michael Rosario <michael.p.rosario@gmail.com>
2024-09-04 07:47:29 +0000

    chore: Update chat view 1 component to display chat messages with markdown support

--- chat-demo/src/app/app.config.ts
+++ chat-demo/src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { provideMarkdown } from 'ngx-markdown';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
-  importProvidersFrom(HttpClientModule)
+  importProvidersFrom(HttpClientModule),
+  provideMarkdown()
   ]
 };
--- chat-demo/src/app/chat-message/chat-message.component.ts
+++ chat-demo/src/app/chat-message/chat-message.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { MarkdownModule } from 'ngx-markdown';
 
 @Component({
   selector: 'app-chat-message',
   standalone: true,
-  imports: [],
+  imports: [MarkdownModule],
   templateUrl: './chat-message.component.html',
   styleUrl: './chat-message.component.scss'
 })
